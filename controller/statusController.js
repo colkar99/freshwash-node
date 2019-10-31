7b8824c79662a699746e0481fd8fcd6accf1c585
colkar99 <colkar99@gmail.com>
2019-10-31 15:53:51 +0530

    aas

--- controller/statusController.js
+++ controller/statusController.js
@@ -1,5 +1,4 @@
 const { Status } = require('../model/status')
-const { mailTransporter } = require('../middleware/mailer');
 exports.createStatus = async (req, res, next) => {
     const status = new Status({
         name: req.body.name,
@@ -7,18 +6,5 @@ exports.createStatus = async (req, res, next) => {
         priorityStatus: req.body.priorityStatus
     })
     const result = await status.save();
-    mailTransporter.sendMail(mailbody(),(err,info)=>{
-        if( err) console.log(err);
-        console.log(info)
-    });
     res.status(201).send(result);
-}
-
-function mailbody() {
-    return {
-        from: process.env.EMAIL_USERNAME,
-        to: 'karthikraj.krishnamoorthy@ebenefitsnetwork.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-    }
 }
