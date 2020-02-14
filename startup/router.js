d3e310df843c61e1bbcc3cdb9af004a040ab4288
colkar99 <colkar99@gmail.com>
2020-02-14 11:36:02 +0530

    as

--- startup/router.js
+++ startup/router.js
@@ -6,10 +6,10 @@ const error = require('../middleware/error');
 const express = require('express');
 module.exports = function (app) {
     app.use(express.json());
-    app.use('/user', userRouter);
-    app.use('/auth', authRouter);
-    app.use('/order', orderRouter);
-    app.use('/status', statusRouter);
+    app.use('api/user', userRouter);
+    app.use('api/auth', authRouter);
+    app.use('api/order', orderRouter);
+    app.use('api/status', statusRouter);
     //Common Error handling function
     app.use(error);
 }
