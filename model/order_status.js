a3a57992f55df5d0e1619666b9f3964fc85f76ae
colkar99 <colkar99@gmail.com>
2020-02-17 11:45:50 +0530

    Created orderStatus table

---
+++ model/order_status.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const orderStatusSchema = new mongoose.Schema({
+    //new user Schema
+    status:{
+        type: Number,
+        required: true
+    },
+    statusDescription:{
+        type: String,
+        required: true
+    }
+},{timestamps: true})
+
+const OrderStatus = mongoose.model('orderStatus', orderStatusSchema);
